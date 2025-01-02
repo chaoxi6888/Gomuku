22bcbd10b862108ff41c48be6c03dcba3bf6f52d
2403246954@qq.com <2403246954@qq.com>
2025-01-02 16:51:57 +0800

    删除了抽卡类

--- card_choice.py
+++
@@ -1,7 +0,0 @@
-class CardChoice:
-    def __init__(self, card, choice):
-        self.card = card
-        self.choice = choice
-
-    def __str__(self):
-        return f"{self.card} {self.choice}"
