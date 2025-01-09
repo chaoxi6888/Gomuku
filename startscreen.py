0173844c2c7715cad46ac882a13bb9647b504c88
2403246954@qq.com <2403246954@qq.com>
2025-01-09 20:31:48 +0800

    新增初始界面，新增开始，设置，退出游戏按钮

--- startscreen.py
+++ startscreen.py
@@ -8,17 +8,21 @@ class StartScreen:
         self.background_image = pygame.image.load('image/游戏主界面.png')
         self.font = pygame.font.Font(None, 74)
         self.buttons = {
-            "start": pygame.Rect(540, 200, 200, 50),
-            "settings": pygame.Rect(540, 300, 200, 50),
-            "quit": pygame.Rect(540, 400, 200, 50)
+            "start": pygame.Rect(390, 400, 286, 98),
+            "settings": pygame.Rect(380, 550, 305, 98),
+            "quit": pygame.Rect(400, 700, 268, 98)
+        }
+        self.button_images = {
+            "start": pygame.image.load('image/游戏开始按钮.png'),
+            "settings": pygame.image.load('image/设置按钮.png'),
+            "quit": pygame.image.load('image/退出按钮.png')
         }
 
     def draw(self):
         self.screen.blit(self.background_image, (0, 0))
         for text, rect in self.buttons.items():
-            pygame.draw.rect(self.screen, (0, 0, 0), rect)
-            button_text = self.font.render(text.capitalize(), True, (255, 255, 255))
-            self.screen.blit(button_text, (rect.x + 20, rect.y + 10))
+            button_image = self.button_images[text]
+            self.screen.blit(button_image, rect.topleft)
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -28,5 +32,5 @@ class StartScreen:
         return None
 
     def show_settings(self):
-        # 显示设置界面
+        # Display settings screen
         pass
